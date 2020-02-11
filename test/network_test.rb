12c0e48fddb6253e0146f57692977afb82e014af
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-11 09:32:22 -0700

    Test created for network
    Testing for attributes and setup.

---
+++ test/network_test.rb
@@ -0,0 +1,44 @@
+require_relative 'test_helper'
+require './lib/character'
+require './lib/show'
+require './lib/network'
+
+class NetworkTest < Minitest::Test
+
+  def setup
+    @nbc = Network.new("NBC")
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+  end
+
+  def test_it_exists
+    assert_instance_of Network, @nbc
+  end
+
+  def test_attributes
+    assert_equal "NBC", @nbc.name
+    assert_equal [], @nbc.shows
+  end
+end
+
+
+
+pry(main)> nbc.add_show(knight_rider)
+
+pry(main)> nbc.add_show(parks_and_rec)
+
+pry(main)> nbc.shows
+# => [#<Show:0x00007fe5f8398970...>, #<Show:0x00007fe5f88b0a20...>]
+
+pry(main)> nbc.main_characters
+# => [#<Character:0x00007f98a4ba8dc8...>]
+
+pry(main)> nbc.actors_by_show
+# => {
+      #<Show:0x00007fe5f8398970...> => ["David Hasselhoff", "William Daniels"],
+      #<Show:0x00007fe5f88b0a20...> => ["Amy Poehler", "Nick Offerman"]
+#    }
