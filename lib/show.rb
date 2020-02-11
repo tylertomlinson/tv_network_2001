1a8215d9dcd3830f6b5c9ceb02f9dabaeb2a0984
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-11 09:27:45 -0700

    All tests passing for Iteration 2

--- lib/show.rb
+++ lib/show.rb
@@ -6,4 +6,16 @@ class Show
     @creator = creator
     @characters = characters
   end
+
+  def total_salary
+    @characters.sum(&:salary)
+  end
+
+  def highest_paid_actor
+    @characters.max_by(&:salary).actor
+  end
+
+  def actors
+    @characters.flat_map(&:actor)
+  end
 end
