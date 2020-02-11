3aeaa88bccb342910a635122aa17a23fef9d263f
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-11 09:34:12 -0700

    Fix setup and rake passing up to now

--- lib/network.rb
+++ lib/network.rb
@@ -1,1 +1,9 @@
+class Network
 
+    attr_reader :name, :shows
+
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+end
--- test/network_test.rb
+++ test/network_test.rb
@@ -9,10 +9,10 @@ class NetworkTest < Minitest::Test
     @nbc = Network.new("NBC")
     @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
-    @knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
     @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
-    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
   end
 
   def test_it_exists
@@ -27,18 +27,18 @@ end
 
 
 
-pry(main)> nbc.add_show(knight_rider)
-
-pry(main)> nbc.add_show(parks_and_rec)
-
-pry(main)> nbc.shows
-# => [#<Show:0x00007fe5f8398970...>, #<Show:0x00007fe5f88b0a20...>]
-
-pry(main)> nbc.main_characters
-# => [#<Character:0x00007f98a4ba8dc8...>]
-
-pry(main)> nbc.actors_by_show
-# => {
-      #<Show:0x00007fe5f8398970...> => ["David Hasselhoff", "William Daniels"],
-      #<Show:0x00007fe5f88b0a20...> => ["Amy Poehler", "Nick Offerman"]
-#    }
+# pry(main)> nbc.add_show(knight_rider)
+#
+# pry(main)> nbc.add_show(parks_and_rec)
+#
+# pry(main)> nbc.shows
+# # => [#<Show:0x00007fe5f8398970...>, #<Show:0x00007fe5f88b0a20...>]
+#
+# pry(main)> nbc.main_characters
+# # => [#<Character:0x00007f98a4ba8dc8...>]
+#
+# pry(main)> nbc.actors_by_show
+# # => {
+#       #<Show:0x00007fe5f8398970...> => ["David Hasselhoff", "William Daniels"],
+#       #<Show:0x00007fe5f88b0a20...> => ["Amy Poehler", "Nick Offerman"]
+# #    }
